2fe151a6390c782559fae6c740a0c9943e574f74
Evilpixi <tatosalvi@gmail.com>
2022-12-11 13:41:42 -0300

    added sprites to debug scene

--- js/src/scene/debug-scene.js
+++ js/src/scene/debug-scene.js
@@ -2,7 +2,10 @@ class DebugScene extends Phaser.Scene
 {
     constructor() { super("DebugScene") }
 
-    preload() { }
+    preload() { 
+        this.load.spritesheet("female", "assets/images/female.png", {frameWidth: 51, frameHeight: 62})
+        this.load.spritesheet("male", "assets/images/male.png", {frameWidth: 51, frameHeight: 62})
+    }
 
     create() 
     {
@@ -27,6 +30,34 @@ class DebugScene extends Phaser.Scene
         }
 
         //this.debugButton1 = new UIButton(this, 100, 100, "deploy", ()=> {console.log("deployed")})
+
+
+        // ------------------ sprites ------------------
+        let frames = []
+        for (let i = 0; i<12; i++) {frames.push(i)}
+        this.anims.create({
+            key: 'female-mis',
+            frames: this.anims.generateFrameNumbers('female', { frames: [0, 1, 2, 1] }),
+            frameRate: 8,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'male-mis',
+            frames: this.anims.generateFrameNumbers('male', { frames: [0, 1, 2, 1] }),
+            frameRate: 8,
+            repeat: -1
+        });
+
+        let female1 = this.add.sprite(50, 50, "female").setScale(4).setOrigin(0)
+        let male1 = this.add.sprite(50, 50, "male").setScale(4).setOrigin(0)
+
+        let male2 = this.add.sprite(250, 50, "male").setScale(4).setOrigin(0)
+        let female2 = this.add.sprite(250, 50, "female").setScale(4).setOrigin(0)
+
+        female1.play("female-mis")
+        female2.play("female-mis")
+        male1.play("male-mis")
+        male2.play("male-mis")
     }
 
     update() { }
